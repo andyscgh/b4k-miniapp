0f089c7b6b4929892f063067ac555e2ad79f2eed
andyscgh <72704412+andyscgh@users.noreply.github.com>
2025-06-21 13:37:12 +0300

    Update constants.ts

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,1 +1,14 @@
-export const API_URL = "https://b4k-backend-production.up.railway.app";
+export const SKILLS = [
+  "Режиссер",
+  "Оператор",
+  "Оператор - постановщик",
+  "Старший инженер",
+  "Светохудожник",
+  "Звукорежиссер",
+  "Инженер пультовой",
+  "Инженер Resolume",
+  "Микрофонный техник",
+  "Инженер трансляции"
+];
+
+export const BACKEND_URL = "https://b4k-backend-production.up.railway.app";
